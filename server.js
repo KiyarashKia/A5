96d7d78196d312e922bdc1e214b1995f019b58df
Kiarash <kkia2@myseneca.ca>
2024-04-11 21:39:11 -0400

    Updated fixed Mongo fixing attempt

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ const connectDB = async () => {
 }
 
 connectDB().then(() => {
-  app.listen(PORT, () => {
+  app.listen(HTTP_PORT, () => {
       console.log("listening for requests");
   })
 })
@@ -65,6 +65,10 @@ function ensureLogin(req, res, next) {
   }
 }
 
+app.get('/', (req, res) => {
+  //res.sendFile(path.join(__dirname, '/views/home.html'));
+  res.render('home');
+  });
 
 Promise.all([legoData.initialize(), authData.initialize()])
   .then(() => {
